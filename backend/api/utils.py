7571d1341ed86d0b4a6c3864b09feb97364cfd58
Антон Уваров <doobydouglas90@gmail.com>
2023-06-09 18:38:20 +0300

    фафыа

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -5,18 +5,19 @@ from rest_framework import status
 class AddAndDelMixin:
 
     def common_action(self, obj, relation, model):
-        if self.request.method == 'POST':
-            if relation.exists():
-                return Response(status=status.HTTP_400_BAD_REQUEST)
-            field_names = [field.name for field in model._meta.fields]
-            action_dict = {
-                field_names[1]: obj,
-                field_names[2]: self.request.user,
-            }
-            model.objects.create(**action_dict)
-            return Response(status=status.HTTP_201_CREATED)
-        if self.request.method == 'DELETE':
-            if relation.exists():
+        if not self.request.user.is_anonymous:
+            if self.request.method == 'POST' and not relation.exists():
+            # if relation.exists():
+            #     return Response(status=status.HTTP_400_BAD_REQUEST)
+                field_names = [field.name for field in model._meta.fields]
+                action_dict = {
+                    field_names[1]: obj,
+                    field_names[2]: self.request.user,
+                }
+                model.objects.create(**action_dict)
+                return Response(status=status.HTTP_201_CREATED)
+            if self.request.method == 'DELETE' and relation.exists():
+            # if relation.exists():
                 relation.delete()
                 return Response(status=status.HTTP_204_NO_CONTENT)
             return Response(status=status.HTTP_400_BAD_REQUEST)
