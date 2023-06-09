725a7109b07198d587e859e5c4834f4bf12184b4
Антон Уваров <doobydouglas90@gmail.com>
2023-06-09 21:14:23 +0300

    ффаыафы

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -12,8 +12,7 @@ class AddAndDelMixin:
                     field_names[1]: obj,
                     field_names[2]: self.request.user,
                 }
-                model.objects.create(**action_dict)
-                model.save()
+                model(**action_dict).save()
                 return Response(status=status.HTTP_201_CREATED)
             elif self.request.method == 'DELETE' and relation.exists():
                 relation.delete()
